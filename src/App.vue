68ec6aabcb27aa8ba7b71f6cebb63d561ef420f1
Edward Tanguay (windows) <edward@tanguay.info>
2024-06-29 12:57:58 +0200

    subtitle

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ import HelloWorld from "./components/HelloWorld.vue";
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You created it!" />
+      <HelloWorld msg="You created it!" subtitle="A great site"/>
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts">
 defineProps<{
-  msg: string
+  msg: string,
+  subtitle: string
 }>()
 </script>
 
 <template>
   <div class="greetings">
     <h1 class="green">{{ msg }}</h1>
+    <h2>{{subtitle}}</h2>
     <h3>
       You’ve successfully created a project with
       <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
