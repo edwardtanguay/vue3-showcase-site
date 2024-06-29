4f9a43c7dab8e2a43c114f0f0247b95ad10b2df9
Edward Tanguay (windows) <edward@tanguay.info>
2024-06-29 13:56:49 +0200

    removed Icons

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,8 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+	<main>
+		<p>This is the main page.</p>
+	</main>
 </template>
