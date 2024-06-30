138c568d1edc37ed7acb71c8ada4a7cc0b3d6b3e
Edward Tanguay (windows) <edward@tanguay.info>
2024-06-30 18:51:45 +0200

    no errors but not showing

---
+++ src/components/SingleTechbook.vue
@@ -0,0 +1,32 @@
+<script lang="ts">
+import { RouterLink } from 'vue-router'
+import type { Techbook } from '@/types'
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+	props: {
+		techbook: {
+			type: {} as () => Techbook,
+			required: true
+		}
+	}
+})
+</script>
+
+<template>
+	<div class="bg-gray-200 p-3 rounded mb-3 min-h-[8rem] flex gap-3">
+		<img :src="`images/${techbook.idCode}.jpg`" alt="book" class="w-[6rem] mdBook" />
+		<section>
+			<div class="bookTitle font-semibold md:text-xl">
+				<img
+					:src="`images/${techbook.idCode}.jpg`"
+					alt="book"
+					class="w-[4rem] float-right m-2 md:hidden"
+				/><RouterLink :to="`techbooks/${techbook.id}`" class="underline">{{
+					techbook.title
+				}}</RouterLink>
+			</div>
+			<div class="bookDescription italic">{{ techbook.description }}</div>
+		</section>
+	</div>
+</template>
--- src/pages/PageTechbooks.vue
+++ src/pages/PageTechbooks.vue
@@ -1,7 +1,7 @@
 <script lang="ts">
 import type { Techbook } from '@/types'
 import _techbooks from '../data/techbooks.json'
-import { RouterLink } from 'vue-router'
+import SingleTechbook from '../components/SingleTechBook.vue'
 
 const techbooks: Techbook[] = structuredClone(_techbooks)
 techbooks.sort((a, b) => (a.title.toLowerCase() > b.title.toLowerCase() ? 1 : -1))
@@ -19,21 +19,9 @@ export default {
 	<section>
 		<p class="mb-3 mt-4 text-xs">There are {{ techbooks.length }} tech books:</p>
 		<template v-for="techbook in techbooks" :key="techbook.id">
-			<div class="bg-gray-200 p-3 rounded mb-3 min-h-[8rem] flex gap-3">
-				<img :src="`images/${techbook.idCode}.jpg`" alt="book" class="w-[6rem] mdBook" />
-				<section>
-					<div class="bookTitle font-semibold md:text-xl">
-						<img
-							:src="`images/${techbook.idCode}.jpg`"
-							alt="book"
-							class="w-[4rem] float-right m-2 md:hidden"
-						/><RouterLink :to="`techbooks/${techbook.id}`" class="underline">{{
-							techbook.title
-						}}</RouterLink>
-					</div>
-					<div class="bookDescription italic">{{ techbook.description }}</div>
-				</section>
-			</div>
+			<p>111</p>
+			<SingleTechbook :techbook="techbook" />
+			<p>222</p>
 		</template>
 	</section>
 </template>
