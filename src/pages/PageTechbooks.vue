c6ddaf3aebc2cc669f0918bea5e7f2fb8c83adea
Edward Tanguay (windows) <edward@tanguay.info>
2024-06-30 18:35:39 +0200

    one link to single works

--- src/pages/PageTechbooks.vue
+++ src/pages/PageTechbooks.vue
@@ -17,27 +17,21 @@ export default {
 <template>
 	<section>
 		<p class="mb-3 mt-4 text-xs">There are {{ techbooks.length }} tech books:</p>
-		<div
-			v-for="techbook in techbooks"
-			:key="techbook.id"
-			class="bg-gray-200 p-3 rounded mb-3 min-h-[8rem] flex gap-3"
-		>
-			<img
-				:src="`images/${techbook.idCode}.jpg`"
-				alt="book"
-				class="w-[6rem] mdBook"
-			/>
-			<section>
-				<div class="bookTitle font-semibold md:text-xl">
-					<img
-						:src="`images/${techbook.idCode}.jpg`"
-						alt="book"
-						class="w-[4rem] float-right m-2 md:hidden"
-					/>{{ techbook.title }}
-				</div>
-				<div class="bookDescription italic">{{ techbook.description }}</div>
-			</section>
-		</div>
+		<template v-for="techbook in techbooks" :key="techbook.id">
+			<div class="bg-gray-200 p-3 rounded mb-3 min-h-[8rem] flex gap-3">
+				<img :src="`images/${techbook.idCode}.jpg`" alt="book" class="w-[6rem] mdBook" />
+				<section>
+					<div class="bookTitle font-semibold md:text-xl">
+						<img
+							:src="`images/${techbook.idCode}.jpg`"
+							alt="book"
+							class="w-[4rem] float-right m-2 md:hidden"
+						/><a :href="`techbooks/${techbook.id}`" class="underline">{{ techbook.title }}</a>
+					</div>
+					<div class="bookDescription italic">{{ techbook.description }}</div>
+				</section>
+			</div>
+		</template>
 	</section>
 </template>
 
