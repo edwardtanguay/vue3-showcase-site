8164b93211b54b609b317e98ff054ce01d438590
Edward Tanguay (windows) <edward@tanguay.info>
2024-06-30 18:31:02 +0200

    cleaned hello test

--- src/pages/PageTechbook.vue
+++ src/pages/PageTechbook.vue
@@ -6,7 +6,6 @@ import { defineComponent, ref } from 'vue'
 
 export default defineComponent({
 	setup() {
-		const test = 'hello'
 		const route = useRoute()
 		const id = ref(route.params.id as string)
 
@@ -15,8 +14,7 @@ export default defineComponent({
 
 		return {
 			techbook,
-			id,
-			test
+			id
 		}
 	}
 })
@@ -24,7 +22,6 @@ export default defineComponent({
 
 <template>
 	<section>
-		<p>[{{ test }}]</p>
 		<div v-if="techbook === undefined">
 			<p>Sorry, no tech book found with id {{ id }}.</p>
 		</div>
