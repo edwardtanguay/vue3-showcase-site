1526e5653364921c5ed6c2f5ccc8d0c0ddffb42e
Edward Tanguay (windows) <edward@tanguay.info>
2024-06-30 18:06:58 +0200

    with defineComponent setup()

--- src/pages/PageTechbook.vue
+++ src/pages/PageTechbook.vue
@@ -1,18 +1,20 @@
 <script lang="ts">
 import type { Techbook } from '@/types'
 import _techbooks from '../data/techbooks.json'
-import { defineComponent } from 'vue';
-
-const id = 18;
-
-const techbooks: Techbook[] = structuredClone(_techbooks)
-const techbook = techbooks.find((m) => m.id === id)
+import { defineComponent } from 'vue'
 
 export default defineComponent({
-	data() {
+	setup() {
+		const test = 'hello'
+		const id = 18
+
+		const techbooks: Techbook[] = structuredClone(_techbooks)
+		const techbook = techbooks.find((m) => m.id === id)
+
 		return {
 			techbook,
-			id
+			id,
+			test
 		}
 	}
 })
@@ -20,11 +22,14 @@ export default defineComponent({
 
 <template>
 	<section>
+		<p>[{{ test }}]</p>
 		<div v-if="techbook === undefined">
 			<p>Sorry, no tech book found with id {{ id }}.</p>
 		</div>
 		<div v-if="techbook">
-			<p>Showing <strong>{{ techbook.title }}</strong> as a single techbook</p>
+			<p>
+				Showing <strong>{{ techbook.title }}</strong> as a single techbook
+			</p>
 		</div>
 	</section>
 </template>
