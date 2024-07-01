d4453ee0f871f5aa3d7209fe83695ca0b70c1b1f
Edward Tanguay (windows) <edward@tanguay.info>
2024-07-01 08:05:03 +0200

    with range

--- src/examples/ExampleCounter.vue
+++ src/examples/ExampleCounter.vue
@@ -3,35 +3,40 @@ import { defineComponent, ref } from 'vue'
 
 export default defineComponent({
 	setup() {
-		const count = ref(10)
-		const changeAmount = ref(2)
+		const count = ref(5)
+		const changeAmount = ref(1)
+		const lowerLimit = 0
+		const upperLimit = 10
 
 		const increment = () => {
 			count.value += Number(changeAmount.value)
-			if(count.value > 20) {
-				count.value = 20;
+			if(count.value > upperLimit) {
+				count.value = upperLimit;
 			}
 		}
 
 		const decrement = () => {
 			count.value -= Number(changeAmount.value)
-			if(count.value < 0) {
-				count.value = 0;
+			if(count.value < lowerLimit) {
+				count.value = lowerLimit;
 			}
 		}
 
-		return { count, increment, decrement, changeAmount }
+		return { count, increment, decrement, changeAmount, lowerLimit, upperLimit }
 	}
 })
 </script>
 
 <template>
 	<div class="flex gap-2">
-		<button :class="`btn w-[2rem] ${count <= 0 ? 'opacity-50' : ''}`" @click="decrement" :disabled="changeAmount <= 0">-</button>
+		<button :class="`btn w-[2rem] ${count <= lowerLimit ? 'opacity-50' : ''}`" @click="decrement" :disabled="count <= lowerLimit">-</button>
 		<p class="text-2xl w-[2rem] text-center">{{ count }}</p>
-		<button :class="`btn w-[2rem] ${count >= 20 ? 'opacity-50' : ''}`" @click="increment" :disabled="changeAmount >= 20">+</button>
+		<button :class="`btn w-[2rem] ${count >= upperLimit ? 'opacity-50' : ''}`" @click="increment" :disabled="count >= upperLimit">+</button>
 		<div>
 		change by <input v-model="changeAmount" class="w-[2rem] text-center"/>
+		</div>
+		<div>
+		allowed range: {{lowerLimit}}-{{upperLimit}}
 		</div>
 	</div>
 </template>
