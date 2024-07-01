ed9bca330929bbdc004b0fb3164d20742fb26812
Edward Tanguay (windows) <edward@tanguay.info>
2024-07-01 23:28:47 +0200

    with keyup.enter

--- src/examples/ExampleLuckyNameGame.vue
+++ src/examples/ExampleLuckyNameGame.vue
@@ -1,20 +1,35 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 
-type UserState = 'guessing' | 'guessed'
+const people = [{ name: 'Joseph' }, { name: 'Ava' }, { name: 'Olivia' }]
 
-const guess = ref('')
-let userState = ref<UserState>('guessing')
+type AppState = 'guessing' | 'nameNotInList' | 'nameInList'
+
+const guessedName = ref('')
+let appState = ref<AppState>('guessing')
+
+const nameIsInList = (name: string): boolean => {
+	return people.find((m) => m.name === name) !== undefined
+}
 
 const handleGuessSubmit = () => {
-	userState.value = 'guessed'
+	if (nameIsInList(guessedName.value)) {
+		appState.value = 'nameInList'
+	} else {
+		appState.value = 'nameNotInList'
+	}
 }
 </script>
 
 <template>
 	<p class="mb-2">Type in a name and press ENTER:</p>
 	<form @submit.prevent="handleGuessSubmit" class="mb-2">
-		<input v-model="guess" @keyup.enter="handleGuessSubmit" />
+		<input v-model="guessedName" @keyup.enter="handleGuessSubmit" />
 	</form>
-	<div v-if="userState === 'guessed'">Your guess was "{{ guess }}".</div>
+	<div v-if="appState === 'nameInList'">
+		Congratulations, {{ guessedName }} is a lucky name! You've won a prize. Will you accept it?
+	</div>
+	<div v-if="appState === 'nameNotInList'">
+		I'm sorry, {{ guessedName }} is not in today's list of lucky names.
+	</div>
 </template>
