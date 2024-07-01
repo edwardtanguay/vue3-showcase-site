20a18ca98bfc3cb6f22d032b9ae0e9e27cad8d63
Edward Tanguay (windows) <edward@tanguay.info>
2024-07-01 23:11:43 +0200

    showing name on ENTER

--- src/examples/ExampleLuckyNameGame.vue
+++ src/examples/ExampleLuckyNameGame.vue
@@ -1,16 +1,20 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 
+type UserState = 'guessing' | 'guessed'
+
 const guess = ref('')
+let userState = ref<UserState>('guessing')
 
 const handleGuessSubmit = () => {
-	console.log(guess.value);
+	userState.value = 'guessed'
 }
 </script>
 
 <template>
 	<p class="mb-2">Type in a name and press ENTER:</p>
-	<form @submit.prevent="handleGuessSubmit">
+	<form @submit.prevent="handleGuessSubmit" class="mb-2">
 		<input v-model="guess" @keyup.enter="handleGuessSubmit" />
 	</form>
+	<div v-if="userState === 'guessed'">Your guess was "{{ guess }}".</div>
 </template>
--- src/types.ts
+++ src/types.ts
@@ -6,3 +6,4 @@ export type Techbook = {
 	yearMonth: string;
 	rank: string
 }
+
