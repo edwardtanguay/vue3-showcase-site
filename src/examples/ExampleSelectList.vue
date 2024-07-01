46e4bd37542c354a56356b85444527f1897d1cd0
Edward Tanguay (windows) <edward@tanguay.info>
2024-07-01 08:51:19 +0200

    colors ok

--- src/examples/ExampleSelectList.vue
+++ src/examples/ExampleSelectList.vue
@@ -1,17 +1,36 @@
 <script lang="ts">
 import { defineComponent, ref } from 'vue'
 
+const _colors = [
+	{
+		id: 1,
+		text: 'green'
+	},
+	{
+		id: 2,
+		text: 'red'
+	},
+	{
+		id: 3,
+		text: 'blue'
+	}
+]
+
 export default defineComponent({
 	setup() {
 		const message = ref('hello')
+		const colors = ref(_colors)
 
-		return {message}
+		return { message, colors }
 	}
 })
 </script>
 
 <template>
-	<div>
-		message: {{message}}
-	</div>
+	<div>message: {{ message }}</div>
+	<ul class="list-disc ml-6">
+		<li v-for="color in colors" :key="color.id">
+			{{ color.text }}
+		</li>
+	</ul>
 </template>
